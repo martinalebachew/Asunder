7dc9259c43cee5f55ee39d9c76efea74b2bfb3b0
Martin Alebachew <martinalebachew@gmail.com>
2023-05-07 14:39:22 +0300

    Fixed response size bug
    - Removed logging class.
    - Fixed response size bug where it was sent as ascii-compatible characters instead of raw bytes.
    - Implemented JSON structure for response.

--- Asunder-Decryptor/src/decryptor.cpp
+++ Asunder-Decryptor/src/decryptor.cpp
@@ -8,55 +8,30 @@
 
 using json = nlohmann::json;
 
-
-class Log {
-private:
-  bool firstLine = true;
-  std:: string logged_text;
-
-public:
-  bool logToStdout = true;
-
-  void append(std::string text) {
-    if (logToStdout) std::cout << text << std::endl;
-    else {
-      if (!firstLine) logged_text += "\n";
-      else firstLine = false;
-
-      logged_text += text;
-    }
-  }
-
-  void flush() {
-    if (logToStdout) return;
-    std::ofstream file("asdlog.txt");
-    file.write(logged_text.c_str(), logged_text.size());
-  }
-
-  ~Log() {
-    flush();
-  }
-};
-
 int main(int argc, char **argv) {
-  Log log;
-  if (argc > 1) log.logToStdout = false;
-
+  // Fetch request, according to the native messaging protocol
   uint32_t inputSize;
   std::cin.read((char*)&inputSize, sizeof(inputSize));
-
   char *inputString = new char[inputSize];
   std::cin.read(inputString, inputSize);
-  log.append("Raw JSON input >>");
-  log.append((std::string)inputString);
-
   json request = json::parse(inputString);
+
+  // Get request fields
   std::string downloadUrl = request["downloadUrl"];
   std::string password = request["password"];
   std::string filename = request["filename"];
 
-  std::string response = R"({"success": true)";
-  std::cout << (uint32_t)response.size();
-  std::cout << response.c_str() << std::flush;
-  log.append("Sent response. Exiting.");
+  // Set response fields
+  json response;
+  response["success"] = true;
+
+  // Send response, according to the native messaging protocol
+  std::string responseString = response.dump(0);
+  uint32_t responseSize(responseString.size());
+  
+  for (int i = 0; i < sizeof(uint32_t); i++) {
+    std::cout << ((char*)&responseSize)[i];
+  }
+
+  std::cout << responseString.c_str() << std::flush;
 }
