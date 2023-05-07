f576aff843b7b01fb22ea4cfe6f08b5b9f69c1f4
Martin Alebachew <martinalebachew@gmail.com>
2023-05-07 14:43:15 +0300

    Implemented decryptor calls
    - Removed PDF downloading code from the service worker.
    - Implemented decryptor calls on the service worker.

--- Asunder-Extension/src/background.ts
+++ Asunder-Extension/src/background.ts
@@ -1,24 +1,11 @@
 // background.ts
 // (C) Martin Alebachew, 2023
 
-let passwordToBlobB64: {[_: string]: string} = {};
-
 chrome.runtime.onConnect.addListener((port) => {
     switch (port.name) {
         case "download_port":
             port.onMessage.addListener(({ bookId, token }: { bookId: string, token: string }) => {
                 downloadBookHandler(bookId, token);
-            });
-            break;
-
-        case "base64":
-            port.onMessage.addListener((password: string) => {
-                port.postMessage({
-                    password: password,
-                    blob_b64: passwordToBlobB64[password]
-                });
-
-                delete passwordToBlobB64[password];
             });
             break;
 
@@ -30,7 +17,7 @@ chrome.runtime.onConnect.addListener((port) => {
 async function downloadBookHandler(bookId: string, token: string) {
     const { licenseId, userId, password } = await getLicense(bookId, token);
     const downloadUrl = await getDownloadUrl(licenseId, userId, bookId, token);
-    await savePdf(downloadUrl, password, `${bookId}.pdf`);
+    await callDecryptor(downloadUrl, password, `${bookId}.pdf`);
 }
 
 async function getLicense(bookId: string, token: string) {
@@ -60,26 +47,12 @@ async function getDownloadUrl(licenseId: string, userId: string, bookId: string,
     return response.DownloadURL;
 }
 
-async function savePdf(downloadUrl: string, password: string, filename: string) {
-    const rawBlob = await (await fetch(downloadUrl, {
-        method: "GET"
-    })).blob();
-
-    const pdfBlob = new Blob([rawBlob], { type: "application/pdf" });
-    const b64 = await blobToBase64(pdfBlob);
-    passwordToBlobB64[password] = JSON.stringify({ blob: b64 });
-
-    // TODO: Replace with chrome.offscreen API
-    await chrome.windows.create({ url: `donwloader/donwloader.html?password=${password}&filename=${filename}`, state: "minimized" });
-}
-
-function blobToBase64(blob: Blob) : Promise<string> {
-    /* Modified version of
-       https://stackoverflow.com/questions/18650168/convert-blob-to-base64
-     */
-    return new Promise((resolve, _) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result as string);
-        reader.readAsDataURL(blob);
-    });
+async function callDecryptor(downloadUrl: string, password: string, filename: string) {
+    chrome.runtime.sendNativeMessage(
+        "com.martinalebachew.asunderdecryptor",
+        { downloadUrl: downloadUrl, password: password, filename: filename },
+        function (response) {
+            if (!response.success) throw `[PDF DECRYPT ERR] Native decryptor failed.`;
+        }
+    );
 }
