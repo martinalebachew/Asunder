524c47732485dfe0a08cd19652183483faa72681
Martin Alebachew <martinalebachew@gmail.com>
2023-05-07 09:39:43 +0300

    Created logging class
    - Created logging class, since Chrome uses stdio to communicate with our program.

--- Asunder Decryptor/src/decryptor.cpp
+++ Asunder Decryptor/src/decryptor.cpp
@@ -2,7 +2,30 @@
 // (C) Martin Alebachew, 2023
 
 #include <iostream>
+#include <fstream>
 #include <boost/filesystem.hpp>
+class Log {
+private:
+  bool firstLine = true;
+  std:: string logged_text;
+
+public:
+  void append(std::string text) {
+    if (!firstLine) logged_text += "\n";
+    else firstLine = false;
+    
+    logged_text += text;
+  }
+
+  void flush() {
+    std::ofstream file("asdlog.txt");
+    file.write(logged_text.c_str(), logged_text.size());
+  }
+
+  ~Log() {
+    flush();
+  }
+};
 
 int main() {
   std::cout << "Hello World!" << std::endl;
