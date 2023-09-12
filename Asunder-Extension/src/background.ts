b1966f313d47507904bb2649b273e0a05eb8e0f4
Martin Alebachew <martinalebachew@gmail.com>
2023-09-12 11:30:01 +0300

    Changed indentation
    - Changed indentation from 4 to 2.

--- Asunder-Extension/src/background.ts
+++ Asunder-Extension/src/background.ts
@@ -2,61 +2,61 @@
 // (C) Martin Alebachew, 2023
 
 chrome.runtime.onConnect.addListener((port) => {
-    switch (port.name) {
-        case "download_port":
-            port.onMessage.addListener(({ bookId, token }: { bookId: string, token: string }) => {
-                downloadBookHandler(bookId, token);
-            });
-            break;
+  switch (port.name) {
+  case "download_port":
+    port.onMessage.addListener(({ bookId, token }: { bookId: string, token: string }) => {
+      downloadBookHandler(bookId, token);
+    });
+    break;
 
-        default:
-            throw `[SW ERROR] Connection on unknown port named ${port.name}.`;
-    }
+  default:
+    throw `[SW ERROR] Connection on unknown port named ${port.name}.`;
+  }
 });
 
 async function downloadBookHandler(bookId: string, token: string) {
-    const { licenseId, userId, password } = await getLicense(bookId, token);
-    const downloadUrl = await getDownloadUrl(licenseId, userId, bookId, token);
-    await callDecryptor(downloadUrl, password, `${bookId}.pdf`);
+  const { licenseId, userId, password } = await getLicense(bookId, token);
+  const downloadUrl = await getDownloadUrl(licenseId, userId, bookId, token);
+  await callDecryptor(downloadUrl, password, `${bookId}.pdf`);
 }
 
 async function getLicense(bookId: string, token: string) {
-    const response = await (await fetch(`https://my.classoos.com/rest_api.php/books/${bookId}/license`, {
-        method: "GET",
-        headers: {
-            Authorization: "Bearer " + token
-        }
-    })).json();
+  const response = await (await fetch(`https://my.classoos.com/rest_api.php/books/${bookId}/license`, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token
+    }
+  })).json();
 
-    const license = response.data[0];
-    const licenseId = license.license_id;
-    const userId = license.user_id;
-    const password = license.password;
-    return { licenseId, userId, password };
+  const license = response.data[0];
+  const licenseId = license.license_id;
+  const userId = license.user_id;
+  const password = license.password;
+  return { licenseId, userId, password };
 }
 
 async function getDownloadUrl(licenseId: string, userId: string, bookId: string, token: string) {
-    const response = await (await fetch(`https://dwb.classoos.com/api/books?LicenseID=${licenseId}&UserID=${userId}&BookID=${bookId}`, {
-        method: "GET",
-        headers: {
-            Authorization: "Bearer " + token
-        }
-    })).json();
+  const response = await (await fetch(`https://dwb.classoos.com/api/books?LicenseID=${licenseId}&UserID=${userId}&BookID=${bookId}`, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token
+    }
+  })).json();
 
-    if (!response.Success) throw `[PDF URL ERR] API denied access with message ${response.ErrorMessage} (Code ${response.ErrorCode})`;
-    return response.DownloadURL;
+  if (!response.Success) throw `[PDF URL ERR] API denied access with message ${response.ErrorMessage} (Code ${response.ErrorCode})`;
+  return response.DownloadURL;
 }
 
 async function callDecryptor(downloadUrl: string, password: string, filename: string) {
-    chrome.runtime.sendNativeMessage(
-        "martinalebachew.asunderdecryptor",
-        { downloadUrl: downloadUrl, password: password, filename: filename },
-        function (response) {
-            if (!response.success) throw `[PDF DECRYPT ERR] Native decryptor failed. ${response.error}`;
+  chrome.runtime.sendNativeMessage(
+    "martinalebachew.asunderdecryptor",
+    { downloadUrl: downloadUrl, password: password, filename: filename },
+    function (response) {
+      if (!response.success) throw `[PDF DECRYPT ERR] Native decryptor failed. ${response.error}`;
 
-            chrome.tabs.create({
-                url: "file://" + response.filename
-            })
-        }
-    );
+      chrome.tabs.create({
+        url: "file://" + response.filename
+      });
+    }
+  );
 }
--- Asunder-Extension/src/content_scripts/bookshelf.ts
+++ Asunder-Extension/src/content_scripts/bookshelf.ts
@@ -6,25 +6,25 @@ const download_port = chrome.runtime.connect({ name: "download_port" });
 const token = localStorage["token"]; // Classoos stores session token on local storage
 
 function onUILoaded() {
-    // A workaround for false-positive UI loaded events from Classoos.
-    try {
-        pinButtonToBookCovers();
-    } catch (error) {
-        setTimeout(onUILoaded, 200);
-    }
+  // A workaround for false-positive UI loaded events from Classoos.
+  try {
+    pinButtonToBookCovers();
+  } catch (error) {
+    setTimeout(onUILoaded, 200);
+  }
 }
 
 function pinButtonToBookCovers() {
-    const bookshelf = document.getElementsByClassName("list box-sizing")[0];
-    for (const book of bookshelf.children) {
-        const coverUrl = book.getElementsByTagName("img")[0].src;
-        const bookId = coverUrl.split("/").pop()!.split("_")[1];
+  const bookshelf = document.getElementsByClassName("list box-sizing")[0];
+  for (const book of bookshelf.children) {
+    const coverUrl = book.getElementsByTagName("img")[0].src;
+    const bookId = coverUrl.split("/").pop()!.split("_")[1];
 
-        const downloadButton = document.createElement("img");
-        downloadButton.src = chrome.runtime.getURL("assets/icon-32.png");
-        downloadButton.addEventListener("click", () => {
-            download_port.postMessage({ bookId: bookId, token: token });
-        });
-        book.appendChild(downloadButton);
-    }
+    const downloadButton = document.createElement("img");
+    downloadButton.src = chrome.runtime.getURL("assets/icon-32.png");
+    downloadButton.addEventListener("click", () => {
+      download_port.postMessage({ bookId: bookId, token: token });
+    });
+    book.appendChild(downloadButton);
+  }
 }
