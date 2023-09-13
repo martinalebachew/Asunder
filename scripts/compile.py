0e25c0ef721c63ddd3707a5fccaa632d9f5e8ec8
Martin Alebachew <martinalebachew@gmail.com>
2023-09-13 18:38:56 +0300

    Added PDFNetC downloading
    - Added PDFNetC downloading to the compilation script.

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,6 +1,18 @@
+from os.path import join, dirname
+from utils.fs import *
 from utils.vcpkg import *
 from utils.prerequisites import *
+from utils.pdfnetc import *
+
+def get_dependencies_dir():
+  scripts_dir = dirname(__file__)
+  project_root = dirname(scripts_dir)
+  decryptor_dir = join(project_root, "decryptor")
+  dependencies_dir = join(decryptor_dir, "dep")
+  return dependencies_dir
+
 
 if __name__ == "__main__":
   check_prerequisites()
   check_packages()
+  get_pdfnetc(get_dependencies_dir())
