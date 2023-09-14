3c742918e87fa0b7e5de5a7596ba2f675a5b31a5
Martin Alebachew <martinalebachew@gmail.com>
2023-09-14 15:04:00 +0300

    Implemented run_build_script()

--- scripts/build_extension.py
+++ scripts/build_extension.py
@@ -1,6 +1,17 @@
 from os.path import join, dirname
+from utils.shell import *
+
 def get_extension_dir():
   scripts_dir = dirname(__file__)
   project_root = dirname(scripts_dir)
   decryptor_dir = join(project_root, "extension")
   return decryptor_dir
+
+
+def run_build_script(extension_dir):
+  return_code, _ = run_shell("npm run build", cwd=extension_dir)
+  if return_code == 0:
+    print_success("Built extension")
+  else:
+    print_error("Failed to build extension")
+    exit()
