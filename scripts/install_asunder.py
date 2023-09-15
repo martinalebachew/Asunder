a1a483fd2df67f489b3c255b16090492388ccab2
Martin Alebachew <martinalebachew@gmail.com>
2023-09-15 13:40:15 +0300

    Updated script to remove previous installation

--- scripts/install_asunder.py
+++ scripts/install_asunder.py
@@ -1,10 +1,13 @@
 from compile_decryptor import *
 from build_extension import *
+from utils.fs import *
 from utils.prerequisites import *
 from shared.prerequisites import *
+from shared.installation import *
 
 def install_asunder():
   check_prerequisites(installation_prerequisites)
+  remove_directory(installation_dir)
 
 
 if __name__ == "__main__":
