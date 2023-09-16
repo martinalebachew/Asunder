75baf913229e03008692b52e8b9c0f783790227f
Martin Alebachew <martinalebachew@gmail.com>
2023-09-16 18:50:41 +0300

    Implemented register_native_host_manifest()

--- scripts/install_asunder.py
+++ scripts/install_asunder.py
@@ -1,4 +1,4 @@
-import json
+import json, platform
 from os.path import join
 from compile_decryptor import *
 from build_extension import *
@@ -23,6 +23,28 @@ def add_key_to_extension_manifest(key_field):
   print_success("Added key to manifest file")
 
 
+def register_native_host_manifest(extension_id):
+  os = platform.system()
+  decryptor_file = "decryptor.exe" if os == "Windows" else "decryptor"
+  decryptor_path = join(installation_dir, decryptor_file)
+
+  manifest = {
+    "name": native_host_identifier,
+    "description": "Asunder Decryptor",
+    "path": f"{decryptor_path}",
+    "type": "stdio",
+    "allowed_origins": [f"chrome-extension://{extension_id}/"]
+  }
+
+  manifest = json.dumps(manifest, indent=2)
+
+  create_directory(native_host_manifest_dir)
+  with open(native_host_manifest_path, "w") as manifest_file:
+    manifest_file.write(manifest)
+
+  print_success("Created native host manifest")
+
+
 def install_asunder():
   check_prerequisites(installation_prerequisites)
   remove_directory(installation_dir)
@@ -30,9 +52,12 @@ def install_asunder():
   generate_keyfile()
   key_field = get_public_key()
   add_key_to_extension_manifest(key_field)
-
+  
   binaries_dir = join(get_decryptor_dir(), "bin")
   copy_directory(binaries_dir, installation_dir, ignore_file_not_found=False)
+ 
+  extension_id = get_extension_id()
+  register_native_host_manifest(extension_id)
 
 
 if __name__ == "__main__":
