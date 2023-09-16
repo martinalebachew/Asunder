e15e4b2f7b7d7e21bc35738d6f4b342d70c5ac2b
Martin Alebachew <martinalebachew@gmail.com>
2023-09-16 18:31:14 +0300

    Added native host constants

--- scripts/shared/installation.py
+++ scripts/shared/installation.py
@@ -1,6 +1,26 @@
 import platform
+from os import join
 from utils.fs import resolve_path
+from utils.logging import *
 
 os = platform.system()
 installation_dir = r"~/.asunder" if os != "Windows" else r"~\.asunder"
 installation_dir = resolve_path(installation_dir)
+
+
+native_host_identifier = "com.martinalebachew.asunder"
+native_host_filename = f"_{native_host_identifier}_.json"
+
+def get_native_host_manifest_dir():
+  if os == "Windows":
+    return join(installation_dir, native_host_filename)
+  elif os == "Darwin":
+    return "~/.config/google-chrome/NativeMessagingHosts"
+  elif os == "Linux":
+    return "~/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+  else:
+    print_error(f"Unsupported OS: {os}")
+    exit()
+
+
+native_host_manifest_path = join(get_native_host_manifest_dir(), native_host_filename)
