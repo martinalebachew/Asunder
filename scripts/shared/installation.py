e823f41eb269f9aa9be0c98f69148e069c8a6b61
Martin Alebachew <martinalebachew@gmail.com>
2023-09-16 17:57:07 +0300

    Updated installation directory
    - Updated installation directory to a sub-directory under the user's home directory (~), since it does not require elevation.

--- scripts/shared/installation.py
+++ scripts/shared/installation.py
@@ -2,5 +2,5 @@ import platform
 from utils.fs import resolve_path
 
 os = platform.system()
-installation_dir = r"/usr/local/etc/Asunder" if os != "Windows" else r"%PROGRAMFILES%\Asunder"
+installation_dir = r"~/Asunder" if os != "Windows" else r"~\Asunder"
 installation_dir = resolve_path(installation_dir)
