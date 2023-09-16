833bdb98d30065f4f68f573cdfb1c4750c8a0ac6
Martin Alebachew <martinalebachew@gmail.com>
2023-09-16 18:02:19 +0300

    Updated installation directory

--- scripts/shared/installation.py
+++ scripts/shared/installation.py
@@ -2,5 +2,5 @@ import platform
 from utils.fs import resolve_path
 
 os = platform.system()
-installation_dir = r"~/Asunder" if os != "Windows" else r"~\Asunder"
+installation_dir = r"~/.asunder" if os != "Windows" else r"~\.asunder"
 installation_dir = resolve_path(installation_dir)
