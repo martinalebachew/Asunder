df845b186c7edb4e29a723d271bc230deeb8cd7b
Martin Alebachew <martinalebachew@gmail.com>
2023-09-16 13:05:51 +0300

    Implemented get_public_key()

--- scripts/crypto.py
+++ scripts/crypto.py
@@ -3,10 +3,23 @@ from utils.logging import *
 
 def generate_keyfile(filename="key.pem"):
   return_code, _ = run_shell(f"openssl genrsa 2048 | openssl pkcs8 -topk8 -nocrypt -out \"{filename}\"")
-  
+
   if return_code == 0:
     print_success(f"Generated keyfile {filename}")
 
   else:
     print_error(f"Failed to generate keyfile {filename}")
+    exit()
+
+
+def get_public_key(filename="key.pem"):
+  return_code, output = run_shell(f"openssl rsa -in {filename} -pubout -outform DER | openssl base64 -A")
+  public_key = output.splitlines()[1]
+
+  if return_code == 0:
+    print_success(f"Calculated public key from {filename}")
+    return public_key
+
+  else:
+    print_error(f"Failed to calculate public key from {filename}")
     exit()
