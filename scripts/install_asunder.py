e31dc869eadf6ec5f3a2249aa347808e0f61c98f
Martin Alebachew <martinalebachew@gmail.com>
2023-09-16 16:48:55 +0300

    Implemented add_key_to_manifest()

--- scripts/install_asunder.py
+++ scripts/install_asunder.py
@@ -1,13 +1,35 @@
+import json
+from os.path import join
 from compile_decryptor import *
 from build_extension import *
 from utils.fs import *
 from utils.prerequisites import *
+from utils.crypto import *
 from shared.prerequisites import *
 from shared.installation import *
+
+def add_key_to_manifest(key_field):
+  dist_dir = join(get_extension_dir(), "dist")
+  manifest_path = join(dist_dir, "manifest.json")
+
+  with open(manifest_path, "r") as manifest_file:
+    manifest = json.load(manifest_file)
+
+  manifest["key"] = key_field
+
+  with open(manifest_path, "w") as manifest_file:
+    json.dump(manifest, manifest_file, indent=2)
+
+  print_success("Added key to manifest file")
+
 
 def install_asunder():
   check_prerequisites(installation_prerequisites)
   remove_directory(installation_dir)
+ 
+  generate_keyfile()
+  key_field = get_public_key()
+  add_key_to_manifest(key_field)
 
 
 if __name__ == "__main__":
