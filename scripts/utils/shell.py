c2272767bff64ce0362430538deb7bd2f4c8795b
Martin Alebachew <martinalebachew@gmail.com>
2023-09-16 17:36:41 +0300

    Fixed run_shell() bug

--- scripts/utils/shell.py
+++ scripts/utils/shell.py
@@ -5,14 +5,13 @@ def run_shell(command, cwd=None, bytes=False):
   process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, text=(not bytes), cwd=cwd)
   stdout, stderr = process.communicate()
 
-  divider = b"\n" if stdout and stderr else b""
-  stdout = stdout if stdout else b""
-  stderr = stderr if stderr else b""
+  empty_string = b"" if bytes else ""
+  newline_char = b"\n" if bytes else "\n"
 
-  divider = divider if bytes else str(divider)
-  stdout = stdout if bytes else str(stdout)
-  stderr = stderr if bytes else str(stderr)
+  stdout = stdout if stdout else empty_string
+  stderr = stderr if stderr else empty_string
 
+  divider = newline_char if stdout and stderr else empty_string
   output = stdout + divider + stderr
   return (process.returncode, output)
 
