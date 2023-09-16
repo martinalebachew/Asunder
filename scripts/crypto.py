4f2043a1e6f1f0f57826e404b7d82c3b6c2ac0a4
Martin Alebachew <martinalebachew@gmail.com>
2023-09-16 14:20:02 +0300

    Updated get_public_key()
    - Implemented helper method to return public key in bytes form.

--- scripts/crypto.py
+++ scripts/crypto.py
@@ -1,5 +1,6 @@
 from utils.shell import *
 from utils.logging import *
+from base64 import b64encode
 
 def generate_keyfile(filename="key.pem"):
   return_code, _ = run_shell(f"openssl genrsa 2048 | openssl pkcs8 -topk8 -nocrypt -out \"{filename}\"")
@@ -12,9 +13,9 @@ def generate_keyfile(filename="key.pem"):
     exit()
 
 
-def get_public_key(filename="key.pem"):
-  return_code, output = run_shell(f"openssl rsa -in {filename} -pubout -outform DER | openssl base64 -A")
-  public_key = output.splitlines()[1]
+def __get_public_key_impl(filename="key.pem"):
+  return_code, output = run_shell(f"openssl rsa -in {filename} -pubout -outform DER", bytes=True)
+  public_key = output.split(b"\r\n")[1]
 
   if return_code == 0:
     print_success(f"Calculated public key from {filename}")
@@ -23,3 +24,8 @@ def get_public_key(filename="key.pem"):
   else:
     print_error(f"Failed to calculate public key from {filename}")
     exit()
+
+
+def get_public_key(filename="key.pem"):
+  public_key = __get_public_key_impl(filename)
+  return b64encode(public_key).decode()
