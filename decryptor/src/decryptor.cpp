c0262e732ed3a6eca35bc6711b8c24b1b7383e17
Martin Alebachew <martinalebachew@gmail.com>
2023-09-18 13:59:44 +0300

    Added basic logging

--- decryptor/src/decryptor.cpp
+++ decryptor/src/decryptor.cpp
@@ -8,8 +8,13 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
+  // Initialize log file
+  std::ofstream log("logs/latest.txt");
+
   // Get request fields
   json request = BrowserIO::FetchRequest();
+  log << "Got request: " << request.dump(2) << std::endl;
+
   std::string downloadUrl = request["downloadUrl"];
   std::string password = request["password"];
   std::string filename = request["filename"];
@@ -17,14 +22,20 @@ int main(int argc, char **argv) {
   // Obtain target paths
   fs::path currentPath(fs::current_path()); // TODO: Replace with user's downloads path
   fs::path outputPath = currentPath / filename;
+  log << "Calculated output path: " << outputPath.string() << std::endl;
 
   // Download PDF
   DataBuffer buffer;
   bool downloaded = DownloadFile(downloadUrl.c_str(), &buffer);
   if (!downloaded) {
+    log << "Failed to download PDF!" << std::endl;
+    log.close();
+
     BrowserIO::SendFailure("Failed to download PDF!");
     return 1;
   }
+  
+  log << "Downloaded PDF of size: " << buffer.GetWritten()<<  "bytes." << std::endl;
 
   // Decrypt PDF
   // TODO: Implement exception handling
@@ -32,19 +43,32 @@ int main(int argc, char **argv) {
   // Disable PDFNet logging by redirecting stdout
   std::streambuf *old = std::cout.rdbuf(nullptr);
 
+  log << "Initializing PDFTron with key: " << PDFTRON_KEY << std::endl;
   InitializePDFTron();
+
   PDF document(buffer.Shrink(), buffer.GetWritten());
+
   if (!document.Decrypt(password)) {
+    log << "Failed to decrypt PDF!" << std::endl;
+    log.close();
+
     BrowserIO::SendFailure("Failed to decrypt PDF!");
     return 1;
   }
 
+  log << "Decrypted PDF." << std::endl;
+
   document.Save(outputPath.string());
+  log << "Saved PDF into " << outputPath.string() << std::endl;
+
   TerminatePDFTron();
   
   // Enable stdout logging for native messaging
   std::cout.rdbuf(old);
 
+  log << "Done." << std::endl;
+  log.close();
+
   BrowserIO::SendSuccess(outputPath.string());
-  return 0;  
+  return 0;
 }
